e7b45f142b57b4832f0a47de8be2f4ae0a0462a1
AluluAndArle <imperialys3@gmail.com>
2024-04-03 16:55:25 +0200

    first commit

---
+++ src/Entity/Pokemon.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PokemonRepository::class)]
+class Pokemon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?float $numero = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $type_2 = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNumero(): ?float
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(float $numero): static
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getType2(): ?string
+    {
+        return $this->type_2;
+    }
+
+    public function setType2(?string $type_2): static
+    {
+        $this->type_2 = $type_2;
+
+        return $this;
+    }
+}
